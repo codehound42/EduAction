43517192ca433b9cf2cbc41bb54c927b8a040f84
codehound42 <codehound42@gmail.com>
2024-03-09 11:24:09 +0000

    Save images in filestore and send public image urls from API endpoint

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,8 @@ import asyncio
 import os
 from supabase import create_client
 import uuid
+import json
+import base64
 
 load_dotenv()
 
@@ -69,20 +71,20 @@ async def yt_link(url: str = Body(...), user_id: str = Body(...)):
         url = url.split("&list")[0]
     video_id = url.split("?v=")[1]
     raw_subtitles = YouTubeTranscriptApi.get_transcript(video_id)
-    processed_subtitles = ""
+    transcript = ""
     for row in raw_subtitles:
         processed_row = re.sub(r'\n+', ' ', row['text'])
-        processed_subtitles += processed_row + " "
+        transcript += processed_row + " "
 
-    save_to_supabase(user_id, processed_subtitles, "yt_links", "txt")
+    save_to_supabase(user_id, transcript, "yt_links", "txt")
 
-    # transcript_summary = generate_transcript_summary(processed_subtitles)
-    return processed_subtitles
+    # transcript_summary = generate_transcript_summary(transcript)
+    return transcript
 
 
 @app.post("/api/flashcards/")
-async def flashcards(input_text: str = Body(...)):
-    flashcards = generate_flashcards(input_text)
+async def flashcards(transcript: str = Body(...), user_id: str = Body(...)):
+    flashcards = generate_flashcards(transcript)
 
     # Creating a list of coroutine objects for each flashcard prompt
     coroutine_tasks = [generate_image(flashcard.image_prompt) for flashcard in flashcards.flashcards]
@@ -94,7 +96,18 @@ async def flashcards(input_text: str = Body(...)):
     answers = [flashcard.answer for flashcard in flashcards.flashcards]
     images = [response.json()["artifacts"][0]["base64"] for response in responses]
 
-    return {"questions": questions, "answers": answers, "images": images}
+    # Save images to supabase
+    image_urls = []
+    for image in images:
+        response = save_to_supabase(user_id, image, "flashcards", "png")
+        supabase_object_path = response.json()["Key"].split("content/")[1]
+        public_image_url = supabase.storage.get_bucket('content').get_public_url(supabase_object_path)
+        image_urls.append(public_image_url)
+
+    flashcard_data = {"questions": questions, "answers": answers, "image_urls": image_urls}
+    save_to_supabase(user_id, flashcard_data, "flashcards", "json")
+
+    return {"questions": questions, "answers": answers, "images": image_urls}
 
 
 @app.post("/api/quiz/")
@@ -113,19 +126,26 @@ async def create_user(username: str = Body(...)):
 def save_to_supabase(user_id, data, bucket_subfolder, file_extension):
     bucket = supabase.storage.get_bucket("content")
 
+    tmp_filename = str(uuid.uuid4()) + f".{file_extension}"
+    file_path = f"/tmp/{tmp_filename}.{file_extension}"
     if file_extension == "txt":
         file_type = "text/html"
+        with open(file_path, "w") as fp:
+            fp.write(data)
     elif file_extension == "png":
         file_type = "image/png"
+        with open(file_path, "wb") as fp:
+            fp.write(base64.b64decode(data))
+    elif file_extension == "json":
+        file_type = "application/json"
+        with open(file_path, "w") as fp:
+           json.dump(data, fp)
     else:
         raise NotImplementedError
 
-    tmp_filename = str(uuid.uuid4()) + f".{file_extension}"
-    file_path = f"/tmp/{tmp_filename}.{file_extension}"
-    with open(file_path, "w") as fp:
-        fp.write(data)
+    response = bucket.upload(path=f"{user_id}/{bucket_subfolder}/{tmp_filename}", file=file_path, file_options={"content-type": file_type})
 
-    bucket.upload(path=f"{user_id}/{bucket_subfolder}/{tmp_filename}", file=file_path, file_options={"content-type": file_type})
+    return response
 
 
 if __name__ == "__main__":
